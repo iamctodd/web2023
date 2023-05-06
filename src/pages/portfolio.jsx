949bff721e587734ea3295a8e189b2c00962642a
C.Todd Lombardo <iamctodd@users.noreply.github.com>
2023-05-06 13:34:34 -0400

    editing v2

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -4,10 +4,8 @@ import Image from 'next/image'
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
 import logoTedx from '@/images/logos/ted.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
+
 
 const projects = [
   {
